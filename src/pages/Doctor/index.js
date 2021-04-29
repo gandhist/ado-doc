9913e2d4a0bd0ab2bab04ace30eae71002469c87
Gandhist <gandhisaputra13@gmail.com>
2021-04-29 21:26:11 +0700

    scaffold doctor

--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { DoctorCategory, HomeProfile, RatedDoctor, NewsItem } from '../../components'
 
 const Doctor = () => {
     return (
         <View>
-            <Text>Doctor</Text>
+            <HomeProfile />
+            <Text>Mau konsultasi dengan siapa hari ini?</Text>
+            <DoctorCategory />
+            <DoctorCategory />
+            <DoctorCategory />
+            <DoctorCategory />
+            <Text>Top rated doctor</Text>
+            <RatedDoctor />
+            <RatedDoctor />
+            <RatedDoctor />
+            <Text>Top rated doctor</Text>
+            <NewsItem />
+            <NewsItem />
+            <NewsItem />
+
         </View>
     )
 }
